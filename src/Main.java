9d1b1f15329e68b129a3922de08bc411080ff82a
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-02-21 14:38:44 +0530

    Add UC2: Option check implemented

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,17 @@ public class Main {
         while (true) {
             System.out.println("\n===== SNAKE & LADDER GAME MENU =====");
             System.out.println("1. Roll a Dice");
+            System.out.println("2. Demonstrate Single Move");
+            System.out.println("8. EXIT");
 
             int choice = sc.nextInt();
 
             switch(choice) {
                 case 1:
                     SnakeAndLadder.displayDiceRoll();
+                    break;
+                case 2:
+                    SnakeAndLadder.singleMoveUseCase();
                     break;
                 default:
                     System.out.println("Invalid choice");
--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,9 +1,30 @@
 public class SnakeAndLadder {
-
+    static final int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
     public static void displayDiceRoll(){
         System.out.println("Dice rolled: "+rollDice());
     }
     public static int rollDice() {
         return (int) (Math.random() * 6) + 1;
+    }
+    public static void singleMoveUseCase(){
+        int position = 0;
+        int dice = rollDice();
+        int option = getGameOption();
+
+        position = updatePlayerPosition(position, dice, option);
+
+        System.out.println("Dice="+dice+ ", Option="+option+", New Position="+position);
+
+    }
+    public static int updatePlayerPosition(int position, int dice, int option) {
+        if (option == LADDER) position += dice;
+        else if (option == SNAKE) position -= dice;
+
+        if (position < 0) position = 0;
+
+        return position;
+    }
+    public static int getGameOption() {
+        return (int) (Math.random() * 3);
     }
 }
